b1d08912146a7afe85f0cbbe1fb0dbb0ceb663f1
stwhh <656179572@qq.com>
2017-10-14 21:35:51 +0800

    添加异常处理，一张图片抓取失败，继续下一个。一个相册抓取失败继续。

--- spider_tbmm.py
+++ spider_tbmm.py
@@ -1,10 +1,12 @@
 # -*-coding:utf-8-*-
-import requests
+import random
 from urllib import request, parse
 import json
 import re
 import ssl
 import os
+
+import time
 
 
 class spider_tbmm:
@@ -12,15 +14,16 @@ class spider_tbmm:
         self.url = 'https://mm.taobao.com/tstar/search/tstar_model.do?_input_charset=utf-8'
 
     # 先打开首页，获取基本信息【分页是post，待实现分页】
-    def get_basicinfo_list(self):
+    def get_basicinfo_list(self,startuserid=0):
         url = self.url
         req = request.Request(url)  # 构造request
         response = request.urlopen(req).read().decode('gbk', 'ignore')
         basic_info = json.loads(response)['data']['searchDOList']
+        print(self.url)
 
-        # for i in basic_info:
-        #     print(i['userId'])
-        return basic_info
+        # for i in basic_info[startuserid:29]:
+        #     print('姓名：{0},userid：{1}'.format(i['realName'],i['userId']))
+        return basic_info[startuserid:len(basic_info)]
 
     # 获取每个用户所有相册id
     def get_album_list(self, userid):
@@ -30,14 +33,15 @@ class spider_tbmm:
 
         # print(response)
         # 特殊字符记得转义
-        pattern = 'a class="mm\-first" href="//mm\.taobao\.com/self/album_photo\.htm\?user_id={0}&album_id=(.*?)&album_flag=0'.format(userid)
+        pattern = 'a class="mm\-first" href="//mm\.taobao\.com/self/album_photo\.htm\?user_id={0}&album_id=(.*?)&album_flag=0'.format(
+            userid)
         albums = re.findall(pattern, response)
         # for i in albums[::2]:
         #     print(i)
-        return albums[::2] # 这里返回的相册信息是重复的所以去重
+        return albums[::2]  # 这里返回的相册信息是重复的所以去重
 
     # 获取所有每个照片id，picid
-    def get_album_detail_list(self, userid, albumid):
+    def get_album_erverpicid_list(self, userid, albumid):
         album_detail_url = 'https://mm.taobao.com/album/json/get_album_photo_list.htm?user_id={}&album_id={}'.format(
             userid, albumid)
         response = request.urlopen(album_detail_url).read().decode('gbk', 'ignore')
@@ -48,12 +52,12 @@ class spider_tbmm:
         # for i in json.loads(response)['picList']:
         #     print(i['userId'] + '-' + i['albumId'] + '-' + i['picId'])
 
-        detailurl = []
+        picids = []
         for i in json.loads(response)['picList']:
             # print(i['picId'])
-            detailurl.append(i['picId'])
+            picids.append(i['picId'])
         # print(detailurl)
-        return detailurl
+        return picids
         # return json.loads(response)['picList']
 
     # 获取每张照片的真实url
@@ -79,33 +83,50 @@ class spider_tbmm:
         # print(response)
         # print(json.loads(response)['photo_url'][2:])
 
-        realurl='http:' + json.loads(response)['photo_url']
+        realurl = 'http:' + json.loads(response)['photo_url']
         print(realurl)
         return realurl
 
     # 启动爬虫
-    def spider_start(self):
+    def spider_start(self,startuserid=0):
         print('爬虫已启动')
         cla = spider_tbmm()
-        for i in cla.get_basicinfo_list():
-            index = 0
-            # 以mm姓名为依据，创建文件夹
-            rootpath = "D:\\淘宝mm\\{}\\".format(i['realName'])
-            cla.mkdir(rootpath)
-            # 创建基本信息
-            print('发现一个mm,姓名:{},身高:{},体重:{}KG'.format(i['realName'], i['height'], i['weight']))
-            mminfo = '姓名：{name}\n城市：{city}\n身高：{height}\n体重：{weight}KG'.format(name=i['realName'], city=i['city'],
-                                                                               height=i['height'], weight=i['weight'])
-            cla.save_mmbasic_info(rootpath + '{}简介.txt'.format(i['realName']), mminfo)
-            # 找到当前mm的所有相册
-            for j in cla.get_album_list(i['userId']):
-                # 找到相册里面的每张图片的真实地址
-                for m in cla.get_album_detail_list(i['userId'], j):
-                    pic_real_url = cla.get_alum_bigpic_url(i['userId'], m)  # 获取到了图片的真实地址开始保存
-                    print('找到了{}的一张照片，正在保存哦'.format(i['realName']))
-                    picpath = (rootpath + '{}{}.jpg').format(i['realName'], index)
-                    cla.save_img(pic_real_url, picpath)
-                    index += 1
+        for i in cla.get_basicinfo_list(startuserid):
+            try:
+                index = 0
+                # 以mm姓名为依据，创建文件夹
+                rootpath = "D:\\淘宝mm\\{}\\".format(i['realName'])
+                cla.mkdir(rootpath)
+                # 创建基本信息
+                print('发现一个mm,姓名:{},身高:{},体重:{}KG'.format(i['realName'], i['height'], i['weight']))
+                mminfo = '姓名：{name}\n城市：{city}\n身高：{height}\n体重：{weight}KG'.format(name=i['realName'], city=i['city'],
+                                                                                   height=i['height'],
+                                                                                   weight=i['weight'])
+                cla.save_mmbasic_info(rootpath + '{}简介.txt'.format(i['realName']), mminfo)
+                # 找到当前mm的所有相册
+                for j in cla.get_album_list(i['userId']):
+                    try:
+                        # 找到相册里面的每张图片的真实地址
+                        for m in cla.get_album_erverpicid_list(i['userId'], j):
+                            pic_real_url = ''
+                            try:
+                                pic_real_url = cla.get_alum_bigpic_url(i['userId'], m)  # 获取到了图片的真实地址开始保存
+                                print('找到了{}的一张照片，正在保存哦'.format(i['realName']))
+                                picpath = (rootpath + '{}{}.jpg').format(i['realName'], index)
+                                cla.save_img(pic_real_url, picpath)
+                                index += 1
+                            except BaseException as e:
+                                print('当前{}的一张照片保存失败，相册id为：{}，照片id：{},照片地址为{}. 错误原因{}'
+                                      .format(i['realName'], j, m, pic_real_url, e))
+                                continue
+                    except BaseException as e:
+                        print('当前{}的相册获取失败，相册id为{}. 错误原因{}'.format(i['realName'], j, e))
+                        time.sleep(random.randint(3,6)) # 爬完一个相册之后等待3-6秒再爬，防止反爬虫
+                        print('正在线程等待几秒钟')
+                        continue
+            except BaseException as e:
+                print('当前{}的基本信息获取失败. 错误原因{}'.format(i['realName'], e))
+                continue
 
     # 创建目录
     def mkdir(self, dirpath):
@@ -141,8 +162,8 @@ class spider_tbmm:
             f.write(data)
 
 
-spider_tbmm().get_album_list(176817195)
+# spider_tbmm().get_album_list(176817195)
 # spider_tbmm().get_album_detail_list(176817195,10000962815)
 # spider_tbmm().get_alum_bigpic_url(176817195, 10003453420)
 # spider_tbmm().get_basicinfo_list()
-# spider_tbmm().spider_start()
+spider_tbmm().spider_start(5)
